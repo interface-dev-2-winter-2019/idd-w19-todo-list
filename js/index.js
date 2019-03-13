5bf181907b28976981d952d86f799ce7510b0eb6
Graphic <graphic@SJH510-00M.local>
2019-03-13 13:57:13 -0400

    Complete new tasks and completion of tasks

--- js/index.js
+++ js/index.js
@@ -1,45 +1,95 @@
-// let tasks = ["walk the dog", "feed the fish", "finish homework"];
-// tasks.push("make dinner")
-
-// console.log(tasks)
-
-//let task = { id: 0, name: "walk the dog", complete: false }
-
-
-// if (tasks[0].complete) {
-//   console.log("This task IS complete");
-// }
-// else {
-//   console.log("This task IS NOT complete")
-// }
-
-
-// DATA SET
+// SAMPLE DATA SET
 let tasks = [
-  { id: 0, name: `Write a "print" function`, complete: true },
-  { id: 1, name: `Add new tasks`, complete: true },
-  { id: 2, name: `Complete/checking tasks`, complete: true },
+  { id: 0, name: `Write a "print" function`, complete: false },
+  { id: 1, name: `Add new tasks`, complete: false },
+  { id: 2, name: `Complete/checking tasks`, complete: false },
   { id: 3, name: `Store the tasks somewhere`, complete: false },
 ];
 
 
 
-// SELECTING DOCUMENT ELEMENTS TO CHANGE
+// DOCUMENT ELEMENTS TO CHANGE
 const allTasks = document.getElementById('alltasks');  // our list of tasks (ol)
+const theTaskForm = document.getElementById('newtask');  // our new task input
+
+
+
+// WHEN THE "NEW TASK" FORM IS SUBMIT
+theTaskForm.addEventListener('submit', event => {
+  // Yes the form was submit!!
+
+  // Add the new task as an object to the tasks Array
+  tasks.push({ 
+    id: tasks.length,
+    name: theTaskForm.task.value,
+    complete: false
+  });
+
+  // Clear out the input field so we can use it again
+  theTaskForm.task.value = '';
+
+  // Reprint the new list
+  printAllTasks();
+
+  // prevent the submit from leaving the page
+  event.preventDefault(); 
+});
+
+
+
+// WHEN A TASK CHECKLIST IS CLICKED
+allTasks.addEventListener('click', event => {
+
+  // Check if the item clicked within the "allTasks" was actually an input item (checkbox!)
+  if (event.target.matches('input')) {
+    
+    // Store the "data-id" value from the checkbox we clicked
+    let theId = event.target.dataset.id;
+
+    // Run once for each task in our "tasks" Array
+    tasks.forEach(oneTask => {
+      // For each one, check if the id from this task (oneTask.id) matches "theId"
+      if (oneTask.id == theId) {
+        oneTask.complete = !oneTask.complete;
+      }
+    })
+
+    // Reprint the whole list again to reflect the new changes to our dataset
+    printAllTasks();
+  }
+
+  /* **NOTE**: In general, it's considered _"bad practice"_ to modify datasets in multiple places
+    in code. A better pattern would be to create a function that specifically meant to modify the 
+    data and pass it a copy of the array with modifications and let that one function overwrite
+    the data and refresh the interface. This prevents having data manipulation happening all over
+    the code, which would be really hard to trace if we had some kind of sync issue. */
+
+});
+
 
 
 // OUTPUT THE RESULTS
-// Map runs once for each Object in the Array
-allTasks.innerHTML = tasks.map(oneTask => 
-  `<li class="task${(oneTask.complete) ? ' complete' : ''}">
-    <label>
-      <input type="checkbox" name="task${oneTask.id}" ${(oneTask.complete) ? ' checked' : ''}>
-      ${oneTask.name}
-    </label>
-  </li>` ).join('');
+function printAllTasks() {
+  // "map()" runs once for each Object in the Array
+  allTasks.innerHTML = tasks.map(oneTask => 
+    `<li class="task${  (oneTask.complete) ? ' complete' : ''  }">
+      <label>
+        <input type="checkbox"
+               data-id="${ oneTask.id }"
+               name="task${ oneTask.id }"
+               ${  (oneTask.complete) ? ' checked' : ''  }>
+        ${ oneTask.name }
+      </label>
+    </li>` ).join('');
+
+  // Note we used the shortcut "ternary" operator to add "if" conditions into the output above.
+  // Ternary format:
+  //    ( trueOrFalseVariableOrCondition ) ? 'condition was true!' : 'condition was false!'
+  // These can go right into our template evaluation blocks: ${ }
+}
 
 
-// var name = "George Brown";
-// My friend's name is Geroge Brown, he's great!
-// "My friend's name is " + name + ", he's great!";
-// `My friend's name is ${name}, he's great!`
+
+// START THE APPLICATION
+// Pring the task list for the first time
+printAllTasks();

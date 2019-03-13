6e1e86ec6430061b3552b47e2e53b482d6934603
Graphic <graphic@SJH510-00M.local>
2019-03-13 13:01:27 -0400

    Dynamic tasks using JS

--- js/index.js
+++ js/index.js
@@ -16,14 +16,30 @@
 
 // DATA SET
 let tasks = [
-  { id: 0, name: "this is live", complete: false },
-  { id: 1, name: "feed the fish", complete: true },
-  { id: 2, name: "finish homework", complete: false }
+  { id: 0, name: `Write a "print" function`, complete: true },
+  { id: 1, name: `Add new tasks`, complete: true },
+  { id: 2, name: `Complete/checking tasks`, complete: true },
+  { id: 3, name: `Store the tasks somewhere`, complete: false },
 ];
+
+
 
 // SELECTING DOCUMENT ELEMENTS TO CHANGE
 const allTasks = document.getElementById('alltasks');  // our list of tasks (ol)
 
 
 // OUTPUT THE RESULTS
-allTasks.innerHTML = tasks.map(t => `<li class="task">${t.name}</li>` ).join('');
+// Map runs once for each Object in the Array
+allTasks.innerHTML = tasks.map(oneTask => 
+  `<li class="task${(oneTask.complete) ? ' complete' : ''}">
+    <label>
+      <input type="checkbox" name="task${oneTask.id}" ${(oneTask.complete) ? ' checked' : ''}>
+      ${oneTask.name}
+    </label>
+  </li>` ).join('');
+
+
+// var name = "George Brown";
+// My friend's name is Geroge Brown, he's great!
+// "My friend's name is " + name + ", he's great!";
+// `My friend's name is ${name}, he's great!`
